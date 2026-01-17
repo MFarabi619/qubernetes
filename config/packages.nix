b225ac5ffdee983720a0c434cccd120708891ef0
Mumtahin Farabi <mfarabi619@gmail.com>
2026-01-17 05:59:06 -0500

    build(config/packages): add deps
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- config/packages.nix
+++ config/packages.nix
@@ -1,9 +1,31 @@
 {
   pkgs,
+  config,
   ...
 }:
 {
-  packages = with pkgs; [
-    platformio
-  ];
+  packages =
+    with pkgs;
+    [
+      platformio
+      mkspiffs-presets.arduino-esp32
+
+      pulumi-esc
+
+      # espup install # . $HOME/export-esp.sh
+      # espup
+      ldproxy
+      esptool
+      espflash
+      esp-generate
+      cargo-espmonitor
+    ]
+    ++ lib.optionals config.languages.c.enable [
+      ninja
+      ccache
+      openocd
+      binsider # binary inspector TUI
+      dfu-util
+      probe-rs-tools # rust-based replacement for stmcli, openocd, etc.
+    ];
 }
